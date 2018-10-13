27289fa8c7b203adb9c9d6e69c2e022c1151e735
Amir Ibrahim <amir.ibrahim@mail.mcgill.ca>
2018-10-13 07:39:12 -0400

    Initial Commit

---
+++ KKarla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KKarla
+//
+//  Created by amir2 on 2018-10-13.
+//  Copyright © 2018 amir2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
