f4204a4384d226dd7c042c70f6f1b8b34dd1418b
Amir Ibrahim <amir.ibrahim@mail.mcgill.ca>
2018-10-19 18:05:29 -0400

    added the tag swift files

---
+++ KKarla/Supporting Files/LabelType.swift
@@ -0,0 +1,79 @@
+//
+//  LabelType.swift
+//  ATagProj
+//
+//  Created by amir2 on 2018-10-19.
+//  Copyright © 2018 amir2. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum LabelType: String {
+    case ageLabel
+    case encountersCountLabel
+    case activeStatusLabel
+    case inactiveStatusLabel
+    case bedsideLocationLabel
+    case diagnosisLabel
+    case tagLabel
+    case genericLabel
+    
+    func formattedStringForLabel(label: String) -> String {
+        return label.lowercased()
+    }
+    
+    var backgroundColorForLabel: UIColor {
+        switch self {
+        case .tagLabel:
+            return #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        //            return UIColor(hue:0.67, saturation:0.3, brightness:0.91, alpha:1)
+        default:
+            return UIColor.clear
+        }
+    }
+    
+    var labelCALayer: CALayer {
+        
+        let layer = CALayer()
+        layer.borderColor = self.colorForLabel.cgColor
+        layer.backgroundColor = self.backgroundColorForLabel.cgColor
+        layer.masksToBounds = true
+        layer.borderWidth = 1.0
+
+        switch self {
+        case .tagLabel:
+            layer.cornerRadius = 10.0
+        default:
+            layer.cornerRadius = 3.0
+        }
+        return layer
+    }
+    
+    var colorForLabel: UIColor {
+        switch self {
+        case .activeStatusLabel:
+            return UIColor(hue:1, saturation:0.5, brightness:0.91, alpha:1)
+        case .inactiveStatusLabel:
+            return UIColor(hue:0.06, saturation:0.4, brightness:0.94, alpha:1)
+        case .encountersCountLabel:
+            return UIColor(hue:0.67, saturation:0.3, brightness:0.91, alpha:1)
+        case .ageLabel:
+            return UIColor(hue:0.28, saturation:0.7, brightness:0.91, alpha:1)
+        case .bedsideLocationLabel:
+            return UIColor(hue:0.09, saturation:1, brightness:0.91, alpha:1)
+        case .diagnosisLabel:
+            return UIColor(hue:0.53, saturation:1, brightness:0.91, alpha:1)
+        case .tagLabel:
+            return .white
+            //return UIColor(hue:0.78, saturation:0.7, brightness:0.91, alpha:1)
+            //        case "accepted":
+            //            return UIColor(hue:0.29, saturation:0.53, brightness:0.8, alpha:1)
+            //        case "rejected":
+        //            return UIColor(hue:0.01, saturation:0.69, brightness:0.82, alpha:1)
+        case .genericLabel:
+            return UIColor(hue:0, saturation:0, brightness:0.69, alpha:1)
+        }
+    }
+    
+}
---
+++ KKarla/Supporting Files/TagStackList.swift
@@ -0,0 +1,52 @@
+//
+//  TagStackList.swift
+//  ATagProj
+//
+//  Created by amir2 on 2018-10-19.
+//  Copyright © 2018 amir2. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TagStackList {
+    var tagStack: UIStackView
+    
+    init(stack: UIStackView){
+        self.tagStack = stack
+    }
+
+}
+
+extension TagStackList {
+    func setLabels(for labels: [(type: LabelType, labelText: String)]) {
+        let font = UIFont.preferredFont(forTextStyle: .caption2)
+        
+        // create an array of labels objects representing the tags
+        // and visually conform to the LabelType enum passed in
+        let labelsArray: [UILabel] = labels.map {
+            let l = NRLabel()
+            l.font = font
+            l.textColor = $0.type.colorForLabel
+            l.text = $0.labelText
+            l.layer.borderColor = $0.type.labelCALayer.borderColor
+            l.layer.borderWidth = $0.type.labelCALayer.borderWidth
+            l.layer.cornerRadius = $0.type.labelCALayer.cornerRadius
+            l.layer.masksToBounds = $0.type.labelCALayer.masksToBounds
+            l.layer.backgroundColor = $0.type.labelCALayer.backgroundColor
+            
+            switch $0.type {
+//            case .tagLabel:
+//                l.textInsets = UIEdgeInsets(top: 5, left: 8, bottom: 5, right: 8)
+            default:
+                l.textInsets = UIEdgeInsets(top: 4, left: 8, bottom: 4, right: 8)
+            }
+            
+            l.clipsToBounds = true
+            return l
+        }
+        
+        self.tagStack.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        labelsArray.forEach { self.tagStack.addArrangedSubview($0) }
+    }
+}
