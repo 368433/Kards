6415904c0abc8268b5947e99b73a7eb9429f424d
Amir Ibrahim <amir.ibrahim@mail.mcgill.ca>
2018-10-19 18:18:39 -0400

    going to move tags to top bar

---
+++ KKarla/Supporting Files/NRLabel.swift
@@ -0,0 +1,64 @@
+//
+//  NRLabel.swift
+//  Karla
+//
+//  Created by amir2 on 2018-04-09.
+//  Copyright © 2018 amir2. All rights reserved.
+//
+
+import UIKit
+
+// Modified from :
+// http://stackoverflow.com/a/21267507
+// and Charter app - add github link
+
+class NRLabel : UILabel {
+    
+    var textInsets = UIEdgeInsets.zero {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+    
+    override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let insetRect = bounds.inset(by: textInsets)
+        let textRect = super.textRect(forBounds: insetRect, limitedToNumberOfLines: numberOfLines)
+        let invertedInsets = UIEdgeInsets(top: -textInsets.top,
+                                          left: -textInsets.left,
+                                          bottom: -textInsets.bottom,
+                                          right: -textInsets.right)
+        return textRect.inset(by: invertedInsets)
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: textInsets))
+    }
+}
+
+@IBDesignable
+extension NRLabel {
+    
+    @IBInspectable
+    var leftTextInset: CGFloat {
+        set { textInsets.left = newValue }
+        get { return textInsets.left }
+    }
+    
+    @IBInspectable
+    var rightTextInset: CGFloat {
+        set { textInsets.right = newValue }
+        get { return textInsets.right }
+    }
+    
+    @IBInspectable
+    var topTextInset: CGFloat {
+        set { textInsets.top = newValue }
+        get { return textInsets.top }
+    }
+    
+    @IBInspectable
+    var bottomTextInset: CGFloat {
+        set { textInsets.bottom = newValue }
+        get { return textInsets.bottom }
+    }
+}
+
+
