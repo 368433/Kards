a9c047b381d2c1bfa5634b271a1a53d04f7a653b
Amir Ibrahim <amir.ibrahim@mail.mcgill.ca>
2018-11-25 11:28:26 -0500

    revision of patient table cell

---
+++ KKarla/PatientsTab/Views/PatientTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  PatientTableViewCell.swift
+//  KKarla
+//
+//  Created by quarticAIMBP2018 on 2018-11-25.
+//  Copyright © 2018 amir2. All rights reserved.
+//
+
+import UIKit
+
+class PatientTableViewCell: UITableViewCell {
+    
+    // MARK: IBOUTLETS
+    @IBOutlet weak var UserImageIcon: UIImageView!
+    @IBOutlet weak var roomNumberLabel: UILabel!
+    @IBOutlet weak var patientNameLabel: UILabel!
+    @IBOutlet weak var patientDetailsLabel: UILabel!
+    @IBOutlet weak var tagListStack: UIStackView!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
