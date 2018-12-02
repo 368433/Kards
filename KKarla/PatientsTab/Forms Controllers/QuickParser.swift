6784c3058930e9a573d14e44b7d4019c9dfb15ad
Amir Ibrahim <amir.ibrahim@mail.mcgill.ca>
2018-12-02 16:22:35 -0500

    implementing the parser. parse year and produce the ramqcode

--- KKarla/PatientsTab/Forms Controllers/QuickParser.swift
+++ KKarla/PatientsTab/Forms Controllers/QuickParser.swift
@@ -16,6 +16,10 @@ class QuickParser {
     var firstName: String?
     var thirdBlock: String?
     var fourthBlock: String?
+    var gender: String?
+    var monthValue: String?
+    var yearValue: String?
+    var dayValue: String?
     var parsedNameRowValue: String?
     var parsedRamqRowValue: String?
     var parsedDateOfBirthValue: Date?
@@ -47,14 +51,60 @@ class QuickParser {
                 firstName = value.containsNumbers ? nil : value
             case 2:
                 thirdBlock = CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: value)) ? String(value.prefix(4)) : nil
+                generateDateOfBirth()
             case 3:
                 fourthBlock = CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: value)) ? String(value.prefix(4)) : nil
+                generateRamqCode()
             default:
                 return
             }
         }
         print([lastName,firstName,thirdBlock,fourthBlock])
+    }
+    
+    
+    private func generateDateOfBirth(){
+        // split the 0000 into two pairs 00 00
+        if thirdBlock?.count == 2 {
+            parseDay(input: thirdBlock)
+        }
         
+        if thirdBlock?.count == 4 {
+            if let str = thirdBlock?.prefix(2) {
+                parseDay(input: String(str) )
+            }
+            if let str = thirdBlock?.suffix(2) {
+                parseMonth(input: String(str) )
+            }
+        }
+        
+//        let firstTwo = thirdBlock?.prefix(2)
+//        let secondTwo = thirdBlock?.suffix(2)
+//
+//        // first 00 is month
+//        if let twoDigits = firstTwo {
+//            month = String(twoDigits)
+//        }
+        
+//        if let second = secondTwo {
+//            let yearGender = String(second)
+//            if let numericYG = Int(yearGender) {
+//                gender = numericYG > 50 ? "F" : "M"
+//                year = (gender == "F") ? String(numericYG - 50) : String(numericYG)
+//            }
+//        }
+        // second 00 is year
+        // if second is > 50, patient is a female, remove 50 to get year
+    }
+    private func parseMonth(input: String?){
+    }
+    private func parseDay(input: String?){
+    }
+    private func parseYear(input: String?){
+        
+    }
+    private func generateRamqCode(){
+        //puttogether all 4 strings
     }
 }
 
