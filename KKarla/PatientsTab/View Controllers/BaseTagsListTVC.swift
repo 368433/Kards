ac18b87aa12753617a64d74c207d61b578915786
Amir Ibrahim <amir.ibrahim@mail.mcgill.ca>
2018-12-02 07:27:50 -0500

    removed unnecessary conditionals applied to model variable

--- KKarla/PatientsTab/View Controllers/BaseTagsListTVC.swift
+++ KKarla/PatientsTab/View Controllers/BaseTagsListTVC.swift
@@ -15,14 +15,6 @@ class BaseTagsListTVC: UITableViewController {
     var predicate: NSPredicate?
     var dataCoordinator = AppDelegate.dataCoordinator
     
-    init(){
-        super.init(nibName:nil, bundle:nil)
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- KKarla/PatientsTab/View Controllers/BaseWorkListsListTVC.swift
+++ KKarla/PatientsTab/View Controllers/BaseWorkListsListTVC.swift
@@ -12,22 +12,14 @@ import UIKit
 class BaseWorkListsListTVC: UITableViewController, Storyboarded{
     
     weak var coordinator: PatientsCoordinator?
-    var model: ClinicalListModel?
+    var model: ClinicalListModel!
     var predicate: NSPredicate?
     var dataCoordinator = AppDelegate.dataCoordinator
-    
-    init(){
-        super.init(nibName:nil, bundle:nil)
-        self.predicate = NSPredicate(format: "isActive == false")
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.predicate = NSPredicate(format: "isActive == false")
         model = ClinicalListModel(modelOutputView: self.tableView, searchPredicate: predicate)
         
         self.tableView.register(UINib(nibName: "ClinicalListTVC", bundle: nil), forCellReuseIdentifier: "cell")
@@ -43,32 +35,31 @@ class BaseWorkListsListTVC: UITableViewController, Storyboarded{
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return model?.resultController.sections?.count ?? 0
+        return model.resultController.sections?.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return model?.resultController.sections![section].numberOfObjects ?? 0
+        return model.resultController.sections![section].numberOfObjects
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ClinicalListTVC
-        cell.configure(workList: model?.resultController.object(at: indexPath))
+        cell.configure(workList: model.resultController.object(at: indexPath))
         return cell
     }
     
     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
 
         let activate = UITableViewRowAction(style: .default, title: "Activate") { (action, indexPath) in
-            if let list = self.model?.resultController.object(at: indexPath) {
+            let list = self.model.resultController.object(at: indexPath)
                 list.isActive = true
                 self.dataCoordinator.saveContext()
-            }
         }
         let edit = UITableViewRowAction(style: .default, title: "Edit") { (action, indexPath) in
             // share item at indexPath
-            if let list = self.model?.resultController.object(at: indexPath) {
+            let list = self.model.resultController.object(at: indexPath)
                 self.coordinator?.showClinicalkListForm(existingList: list)
-            }
+            
         }
         activate.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
         edit.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
@@ -79,7 +70,7 @@ class BaseWorkListsListTVC: UITableViewController, Storyboarded{
         
         let searchKP = [Patient.activeListSKP, Patient.signedOffListSKP, Patient.transferredListSKP]
         let leftExpressions = searchKP.compactMap { NSExpression(forKeyPath: $0) }
-        let tableDerivedExpression = NSExpression(forConstantValue: model?.resultController.object(at: indexPath))
+        let tableDerivedExpression = NSExpression(forConstantValue: model.resultController.object(at: indexPath))
         let comparisonPredicates = leftExpressions.compactMap {
             NSComparisonPredicate(leftExpression: $0,
                                   rightExpression: tableDerivedExpression,
